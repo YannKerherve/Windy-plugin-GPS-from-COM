272495919353599686586a92ed2769023e44bdf4
Yann Kerherve <ya.kerherve@gmail.com>
2024-12-23 19:23:40 +0100

    Update pluginConfig.ts

--- src/pluginConfig.ts
+++ src/pluginConfig.ts
@@ -2,7 +2,7 @@ import type { ExternalPluginConfig } from '@windy/interfaces';
 
 const config: ExternalPluginConfig = {
     name: 'windy-plugin-gpsfromcom',
-    version: '0.1.9',
+    version: '0.1.10',
     icon: '🛰️',
     title: 'GPS from COM',
     description: 'GPS Position from COM port.',
